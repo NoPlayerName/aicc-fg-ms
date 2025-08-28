fe6f0f8bffded7a4c91cb50b93b4881df0da68e3
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2025-08-28 13:46:29 +0700

    update slicing

--- resources/views/layouts/auth/app.blade.php
+++ resources/views/layouts/auth/app.blade.php
@@ -3,6 +3,10 @@
 
 <head>
     <title>{{ $title ?? 'Packing Palet' }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
     @include('components.head')
 
 </head>
