0b9e003bd2934eac5cba3f2c21537755d8c4390e
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-04 09:11:32 +0700

    migration create tbl search rack

---
+++ database/migrations/2025_09_04_090320_create_tb_search_rack.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_search_rack', function (Blueprint $table) {
+            $table->string('part_no', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_search_rack');
+    }
+};
