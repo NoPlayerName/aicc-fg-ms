369fd6451a404ca970b752afbd82cf19525261b0
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-09 08:21:40 +0700

    create table tb_stock_in

---
+++ database/migrations/2025_09_04_095535_create_tb_stock_in.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_stock_in', function (Blueprint $table) {
+            $table->id();
+            $table->string('part_no', 50)->nullable()->index();
+            $table->string('part_name', 250)->nullable()->index();
+            $table->string('pallet_no', 100)->nullable()->index();
+            $table->decimal('qty', 10, 2)->nullable();
+            $table->string('rack_no', 10)->nullable()->index();
+            $table->tinyInteger('status')->nullable()->index();
+            $table->string('desc', 150)->nullable();
+            $table->string('created_by', 50)->nullable()->index();
+            $table->timestamps();
+
+            $table->foreign('pallet_no')->references('pallet_no')->on('tb_pallet')->onDelete('set null')->onUpdate('cascade');
+            $table->foreign('rack_no')->references('rack_no')->on('tb_rack')->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_stock_in');
+    }
+};
