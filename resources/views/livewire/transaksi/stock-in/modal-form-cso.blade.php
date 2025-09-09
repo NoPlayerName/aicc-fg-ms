e3c011d2fd477b94d665f48beee796773b08260d
muhammad ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-09 08:23:26 +0700

    add new livewire view form-cso

---
+++ resources/views/livewire/transaksi/stock-in/modal-form-cso.blade.php
@@ -0,0 +1,66 @@
+<div wire:ignore.self class="modal fade" id="modal-form-cso" tabindex="-1" aria-labelledby="modal-form-cso-label"
+    aria-hidden="false">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-form-cso-label">Form CSO</h5>
+                {{-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> --}}
+            </div>
+            <div class="modal-body">
+                <form wire:submit.prevent="save">
+
+                    <div class="form-group">
+                        <label>ID Pallet</label>
+                        <input type="text" wire:model.defer="id_pallet" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Part No</label>
+                        <input type="text" wire:model.defer="part_no" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Part Name</label>
+                        <input type="text" wire:model.defer="part_name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Qty</label>
+                        <input type="number" wire:model.defer="qty" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>No Pallet</label>
+                        <input type="text" wire:model.defer="no_pallet" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>No Rak</label>
+                        <input type="text" wire:model.defer="no_rak" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Part Trial</label>
+                        <input type="checkbox" wire:model.defer="part_trial" class="form-control">
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- @push('scripts')
+    <script>
+        $(document).on("livewire:navigated", function() {
+            Livewire.on('showModal', () => {
+                $('#modal-form-snp').modal('show');
+            });
+
+            Livewire.on('hideModal', () => {
+                $('#modal-form-snp').modal('hide');
+            });
+        });
+    </script>
+@endpush --}}
