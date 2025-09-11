298d19d2394007ebaf98621d1d4a4fbc848fcf19
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-11 16:11:39 +0700

    new component form update

---
+++ app/Http/Livewire/Transaksi/StockOut/FormUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Transaksi\StockOut;
+
+use Livewire\Component;
+
+class FormUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.transaksi.stock-out.form-update');
+    }
+}
