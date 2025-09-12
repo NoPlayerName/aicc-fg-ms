e8918add0b69a7dd4520a297f3a21dd11a83e8c2
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-12 14:38:04 +0700

    add rack livewire

---
+++ app/Http/Livewire/MasterData/Rack.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\MasterData;
+
+use Livewire\Component;
+
+class Rack extends Component
+{
+    public function render()
+    {
+        return view('livewire.master-data.rack');
+    }
+}
---
+++ resources/views/livewire/master-data/rack.blade.php
@@ -0,0 +1,147 @@
+@push('style')
+<!-- DataTables -->
+<link href={{ asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }} rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet"
+    type="text/css" />
+<link href="{{ asset('assets/libs/datatables.net-select-bs4/css/select.bootstrap4.min.css') }}" rel="stylesheet"
+    type="text/css" />
+
+<!-- Responsive datatable examples -->
+<link href={{ asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }} rel="stylesheet"
+    type="text/css" />
+@endpush
+
+<div class="page-content">
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-flex align-items-center justify-content-between">
+                    <h4 class="mb-0">Rack</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Master</a></li>
+                            <li class="breadcrumb-item active">RACK</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <div class="col-12" wire:ignore>
+            <div class="card">
+                <div class="card-body">
+
+                    {{-- <h4 class="card-title">Stock</h4> --}}
+
+                    <div class="mb-3 d-flex justify-content-start">
+                        <button class="btn btn-primary btn-md mr-2" wire:click="openModal"><i
+                                class="far fa-plus-square"></i> Add
+                            Rack</button>
+                        <div id="custom-buttons"></div>
+                    </div>
+                    <div style="max-width: auto; overflow-x: auto;">
+                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Age</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Tiger Nixon</td>
+                                    <td>System Architect</td>
+                                    <td>Edinburgh</td>
+                                    <td>61</td>
+                                    <td>2011/04/25</td>
+                                    <td>$320,800</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- container-fluid -->
+    {{-- Modal reusable --}}
+    <x-modals.modal id="custModal" title="{{ $id ? 'Edit Customer' : 'Add Customer' }}">
+        {{-- isi modal bebas, bisa form Livewire atau biasa --}}
+        <form wire:submit.prevent="save">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" wire:model="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Inisial</label>
+                <input type="text" wire:model="initial" class="form-control">
+            </div>
+
+            <x-slot name="footer">
+                <button type="submit" class="btn btn-primary">Save</button>
+            </x-slot>
+        </form>
+    </x-modals.modal>
+</div>
+@push('scripts')
+<!-- Required datatable js -->
+<script src={{ asset('assets/libs/datatables.net/js/jquery.dataTables.min.js') }}></script>
+<script src={{ asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}></script>
+
+<!-- Buttons examples -->
+<script src="{{ asset('assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/libs/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('assets/libs/pdfmake/build/pdfmake.min.js') }}"></script>
+<script src="{{ asset('assets/libs/pdfmake/build/vfs_fonts.js') }}"></script>
+<script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('assets/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
+
+
+{{-- datatable init js --}}
+{{-- <script src={{ asset('assets/js/pages/datatables.init.js') }}></script> --}}
+{{-- <script src="{{ asset('assets/js/app.js') }}"></script> --}}
+
+<script>
+    $(document).on("livewire:init", () => {
+            initTable();
+            Livewire.on('open-modal', () => {
+                $('#custModal').modal('show');
+            });
+        });
+
+        function initTable() {
+
+            let table = $('#datatable-buttons').DataTable({
+                // searching: false,
+                responsive: true,
+                lengthChange: false,
+                autoWidth: false,
+                dom: 'Bfrtip',
+                buttons: [{
+                        extend: 'excel',
+                        className: 'btn btn-success',
+                        text: '<i class="fas fa-file-excel"></i> Export Excel'
+                    },
+
+                ]
+            });
+
+            // Pindahkan tombol ke div custom
+            table.buttons().container().appendTo('#custom-buttons');
+        }
+</script>
+@endpush
