b57fc2ac0613c1b87a9614b25679160744e2f1d5
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-13 09:24:36 +0700

    add new component form edit pallet

---
+++ app/Http/Livewire/MasterData/Pallet/FormEditPallet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\MasterData\Pallet;
+
+use Livewire\Component;
+
+class FormEditPallet extends Component
+{
+    public function render()
+    {
+        return view('livewire.master-data.pallet.form-edit-pallet');
+    }
+}
