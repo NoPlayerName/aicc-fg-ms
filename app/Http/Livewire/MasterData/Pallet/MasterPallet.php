c8205b7d3853cb36dd85fae8a1080030fa180c2d
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-13 09:25:01 +0700

    add function for delete confirm

--- app/Http/Livewire/MasterData/Pallet/MasterPallet.php
+++ app/Http/Livewire/MasterData/Pallet/MasterPallet.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Livewire\MasterData\Pallet;
 
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class MasterPallet extends Component
 {
+    public function deleteConfirm($id){
+        $this->dispatch('delete-confirm', id : $id);
+        
+    }
+  
+    public function delete($id)
+    {
+        dd($id);
+        // kirim event feedback ke frontend
+        // $this->dispatch('deleted');
+    }
     public function render()
     {
         return view('livewire.master-data.pallet.master-pallet');
