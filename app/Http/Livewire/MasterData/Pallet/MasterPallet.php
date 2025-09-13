9b234e14c7bd1902297a6a12326ab86d3f48ada0
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-13 08:11:03 +0700

    add new component livewire master pallet

---
+++ app/Http/Livewire/MasterData/Pallet/MasterPallet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\MasterData\Pallet;
+
+use Livewire\Component;
+
+class MasterPallet extends Component
+{
+    public function render()
+    {
+        return view('livewire.master-data.pallet.master-pallet');
+    }
+}
