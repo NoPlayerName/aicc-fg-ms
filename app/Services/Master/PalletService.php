9ac11d600aad1bccf31190f90eaf9c2e67a405bc
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-16 11:05:53 +0700

    add service pallet

---
+++ app/Services/Master/PalletService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Master;
+
+use App\Repositories\Pallet\PalletRepositoryInterface;
+use Illuminate\Http\Request;
+
+
+class PalletService
+{
+    protected $palletRepository;
+
+    public function __construct(PalletRepositoryInterface $palletRepository)
+    {
+        $this->palletRepository = $palletRepository;
+    }
+
+
+    public function getPallet()
+    {
+        return $this->palletRepository->getAll();
+    }
+
+
+    public function savePallet($data)
+    {
+        return $this->palletRepository->create($data);
+    }
+
+    public function updatePallet($id, $data)
+    {
+        return $this->palletRepository->update($id, $data);
+    }
+
+    public function getPalletById($id)
+    {
+        return $this->palletRepository->getbyId($id);
+    }
+
+    public function deletePallet($id)
+    {
+        return $this->palletRepository->delete($id);
+    }
+}
