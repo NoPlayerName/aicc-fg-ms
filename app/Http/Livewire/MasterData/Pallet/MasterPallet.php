04d2f3d0ce3f1fb7b3fc33bad6f9cf95793046b9
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-16 10:32:07 +0700

    add mthod edit, and delete

--- app/Http/Livewire/MasterData/Pallet/MasterPallet.php
+++ app/Http/Livewire/MasterData/Pallet/MasterPallet.php
@@ -2,22 +2,42 @@
 
 namespace App\Http\Livewire\MasterData\Pallet;
 
+use App\Http\Livewire\BaseLivewireComponent;
+use App\Services\Master\PalletService;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-class MasterPallet extends Component
+class MasterPallet extends BaseLivewireComponent
 {
-    public function deleteConfirm($id){
-        $this->dispatch('delete-confirm', id : $id);
-        
+    public $form = [
+        'pallet_no'   => '',
+        'name' => '',
+        'pallet_type' => '',
+        'color'       => '',
+        'is_active'   => true,
+    ];
+
+    public function edit($id)
+    {
+        $this->form = app(PalletService::class)->getPalletById($id);
+
+        $this->dispatch('editForm', data: $this->form, id: $id)->to(FormEditPallet::class);
     }
-  
+
+
+    public function deleteConfirm($id)
+    {
+        $this->dispatch('delete-confirm', id: $id);
+    }
+
+
+
     public function delete($id)
     {
-        dd($id);
-        // kirim event feedback ke frontend
-        // $this->dispatch('deleted');
+        app(PalletService::class)->deletePallet($id);
     }
+
+
     public function render()
     {
         return view('livewire.master-data.pallet.master-pallet');
