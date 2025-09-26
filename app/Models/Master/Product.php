762765bb02d331df9cbc4aae997dd403c4bce3c3
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-09-26 17:39:07 +0700

    create model product

---
+++ app/Models/Master/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Master;
+
+use App\Models\BaseModel;
+
+class Product extends BaseModel
+{
+    protected $table = 'tb_product';
+
+    protected $fillable = [
+        'part_no',
+        'std_packing',
+        'min_stock',
+        'max_stock',
+        'without_pallet',
+        'group_id',
+    ];
+
+    public function group()
+    {
+        return $this->belongsTo(RackGroup::class, 'group_id');
+    }
+
+    public function prodName()
+    {
+        return $this->belongsTo(VProduct::class, 'part_no', 'part_no');
+    }
+}
