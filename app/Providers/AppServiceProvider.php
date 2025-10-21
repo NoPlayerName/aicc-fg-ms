90686f79d362cc36673b44faf3a584144b815a08
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-10-21 07:43:18 +0700

    add livewire set route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $basePath = parse_url(config('app.url'), PHP_URL_PATH) ?? '';
 
+        // Set route JS Livewire secara dinamis
+        Livewire::setScriptRoute(function ($handle) use ($basePath) {
+            return Route::get("{$basePath}/livewire/livewire.js", $handle);
+        });
+
+        // Set route update Livewire secara dinamis
+        Livewire::setUpdateRoute(function ($handle) use ($basePath) {
+            return Route::post("{$basePath}/livewire/update", $handle);
+        });
     }
 }
