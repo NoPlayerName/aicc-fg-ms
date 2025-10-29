5e1a4c244843bb9849f905ac9669872a218b0c27
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2025-10-29 07:58:16 +0700

    change column display

--- resources/views/livewire/stock-change/stock-change.blade.php
+++ resources/views/livewire/stock-change/stock-change.blade.php
@@ -35,7 +35,7 @@
         <div class="row">
             <div class="col-12">
                 <div id="accordion" class="custom-accordion">
-    
+
                     <div class="card mb-1">
                         <a href="#collapseTwo" class="text-dark " data-toggle="collapse" aria-expanded="false"
                             aria-controls="collapseTwo">
@@ -46,7 +46,7 @@
                                 </h6>
                             </div>
                         </a>
-    
+
                         <div id="collapseTwo" class="collapse " aria-labelledby="headingTwo" data-parent="#accordion"
                             wire:ignore.self>
                             <div class="card-body">
@@ -57,20 +57,21 @@
                                                 <div class="input-daterange input-group" data-provide="datepicker"
                                                     data-date-format="yyyy-mm-dd" data-date-autoclose="true">
                                                     <input required type="text" class="form-control" autocomplete="off"
-                                                        placeholder="Start Date" wire:model="startDate" id="s_start_date" />
+                                                        placeholder="Start Date" wire:model="startDate"
+                                                        id="s_start_date" />
                                                     <input required type="text" class="form-control" autocomplete="off"
                                                         placeholder="End Date" wire:model='endDate' id="s_end_date" />
                                                 </div>
                                             </div>
                                         </div>
-    
+
                                         <div class="col-lg-4">
                                             <div class="form-group">
                                                 <input type="text" value="" wire:model="Search" id="search"
                                                     placeholder="Search" class="form-control">
                                             </div>
                                         </div>
-    
+
                                         <div class="col-lg-4">
                                             <button id="filter_btn" type="submit"
                                                 class="btn btn-primary waves-effect waves-light">
@@ -78,23 +79,24 @@
                                             </button>
                                         </div>
                                     </div>
-    
+
                                 </form>
                             </div>
-    
-    
-    
+
+
+
                         </div>
                     </div>
                 </div>
                 <div class="card" wire:ignore>
                     <div class="card-body">
-    
+
                         {{-- <h4 class="card-title">Stock</h4> --}}
-    
+
                         <div style="max-width: auto; overflow-x: auto;">
-    
-                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap"
+
+                            <table id="datatable-buttons"
+                                class="table table-striped table-bordered dt-responsive nowrap"
                                 style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                                 <thead>
                                     <tr>
@@ -106,11 +108,11 @@
                                         <th>Cutomer</th>
                                         <th>Created By</th>
                                         <th>Description</th>
-    
+
                                     </tr>
                                 </thead>
                             </table>
-    
+
                         </div>
                     </div>
                 </div>
@@ -180,8 +182,8 @@ function iniTable() {
         columns: [
             {data: 'form_no', orderable: false},
             {data: 'pallet_no', orderable: false},
-            {data: 'product_code', orderable: false},
             {data: 'part_no', orderable: false},
+            {data: 'part_name', orderable: false},
             {data: 'qty', orderable: false, render: (data) => {
                 return parseInt(data);
             }},
